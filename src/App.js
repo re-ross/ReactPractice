74b1556fca38f3b7d7bb8b2c2e1607b56bf098b7
Ryan Ross <ryanross@Bretts-MBP.attlocal.net>
2022-02-08 15:30:06 -0600

    explored using State to increment, to change color, and to make fake tweets

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import Tweet from "./Tweet";
 
 function App() {
+  const [users, setUsers] = useState([
+    { name: "Ryan", message: "Hey!" },
+    { name: "Jon", message: "I am Jon Snow" },
+    { name: "Traversy", message: "I am awesome" },
+  ]);
+
   return (
     <div className="app">
-      <h1>hello react</h1>
-      <Tweet />
+      {users.map((user) => (
+        <Tweet name={user.name} message={user.message} />
+      ))}
     </div>
   );
 }
--- src/Tweet.js
+++ src/Tweet.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./App.css";
 
-function Tweet() {
+function Tweet({ name, message }) {
   return (
     <div className="tweet">
-      <h3>Name</h3>
-      <p>Random Tweet</p>
+      <h3>{name}</h3>
+      <p>{message}</p>
       <h3>Number of Likes</h3>
     </div>
   );
