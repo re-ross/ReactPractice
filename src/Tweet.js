a7f303f8c78e6986c15062335c87397a86f399f9
Ryan Ross <ryanross@Bretts-MBP.attlocal.net>
2022-02-08 08:53:15 -0600

    added Tweet.js and basic app styling

---
+++ src/Tweet.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+
+function Tweet() {
+  return (
+    <div className="tweet">
+      <h3>Name</h3>
+      <p>Random Tweet</p>
+      <h3>Number of Likes</h3>
+    </div>
+  );
+}
+
+export default Tweet;
+
+// timestamp 25:40
